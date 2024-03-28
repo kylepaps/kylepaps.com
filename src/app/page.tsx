afa0b1e393d53e9b039c862387082457fc2061e6
Kyle Papizewski <kyle@Kyles-MacBook-Pro.local>
2024-03-28 19:48:22 -0400

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,192 @@
+import { ExternalLinkIcon } from "@radix-ui/react-icons";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
+    <main className="flex items-center w-full align-middle justify-center">
+      <div className="w-4/5 sm:w-3/4 md:w-2/3 lg:w-1/3 flex flex-col gap-y-16 py-16">
+        <div className="flex flex-row justify-start items-center gap-x-4">
+          <div className="h-fit w-24">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src="/profile.jpg"
+              alt="profile"
+              className="rounded-3xl"
+              width={500}
+              height={500}
             />
-          </a>
+          </div>
+          <div className="flex flex-col">
+            <span className="scroll-m-20 text-lg tracking-tight">
+              Kyle Papizewski
+            </span>
+            <span className="text-sm">Software Developer 🤠</span>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+        <div className="flex flex-col gap-y-6">
+          <span className="text-sm">About</span>
+          <div className="flex flex-col gap-y-4">
+            <span className="text-muted-foreground text-sm">
+              I&#39;m a software developer located in Toronto. Currently I&#39;m
+              working with MLSE as a Junior Backend Engineer, where I&#39;m able
+              to build applications for the Toronto Maple Leafs, Toronto
+              Raptors, and associated fans. Aside from software development, I
+              enjoy running, golf, basketball, and travel. In 2023 I ran my
+              first marathon. This year I&#39;m training for the Toronto
+              Waterfront Marathon with a 3:45 goal.
             </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          </div>
+        </div>
+        <div className="flex flex-col gap-y-6">
+          <span className="text-sm">Work Experience</span>
+          <div className="flex flex-col gap-y-4">
+            <div className="flex flex-row justify-start">
+              <div className="w-96">
+                <span className="text-muted-foreground text-sm">
+                  Apr. 2023 - Present
+                </span>
+              </div>
+              <div className="flex flex-col gap-y-1 w-full">
+                <div className="flex flex-row gap-x-2">
+                  <div className="text-sm">Junior Backend Engineer</div>
+                  <Link
+                    href={"https://www.mlsedigital.com/"}
+                    className="flex items-center"
+                  >
+                    <ExternalLinkIcon className="flex self-center hover:text-muted-foreground" />
+                  </Link>
+                </div>
+                <span className="text-muted-foreground text-xs">
+                  Maple Leafs Sports & Entertainment Ltd.
+                </span>
+              </div>
+            </div>
+            <div className="flex flex-row justify-start">
+              <div className="w-96">
+                <span className="text-muted-foreground text-sm w-96 pr-4">
+                  Jan. 2023 - Apr. 2023
+                </span>
+              </div>
+              <div className="flex flex-col gap-y-1 w-full">
+                <span className="text-sm">IT Support Technician</span>
+                <span className="text-muted-foreground text-xs">
+                  Maple Leafs Sports & Entertainment Ltd.
+                </span>
+              </div>
+            </div>
+            <div className="flex flex-row justify-start">
+              <div className="w-96">
+                <span className="text-muted-foreground text-sm w-96 pr-4">
+                  Sept. 2022 - Dec. 2023
+                </span>
+              </div>
+              <div className="flex flex-col gap-y-1 w-full">
+                <span className="text-sm">IT Generalist Intern</span>
+                <span className="text-muted-foreground text-xs">
+                  Maple Leafs Sports & Entertainment Ltd.
+                </span>
+              </div>
+            </div>
+            <div className="flex flex-row justify-start">
+              <div className="w-96">
+                <span className="text-muted-foreground text-sm w-96 pr-4">
+                  Jan. 2022 - June. 2022
+                </span>
+              </div>
+              <div className="flex flex-col gap-y-1 w-full">
+                <span className="text-sm">Technical Support Specialist</span>
+                <span className="text-muted-foreground text-xs">
+                  Wilfrid Laurier University
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-y-6">
+          <span className="text-sm">Projects</span>
+          <div className="flex flex-col gap-y-4">
+            <div className="flex flex-row justify-start">
+              <div className="w-96">
+                <span className="text-muted-foreground text-sm">Ongoing</span>
+              </div>
+              <div className="flex flex-col gap-y-1 w-full">
+                <div className="flex flex-row gap-x-2">
+                  <span className="text-sm">Resume Wizard</span>
+                  <Link
+                    href={"https://www.resumewizard.app/"}
+                    className="flex items-center"
+                  >
+                    <ExternalLinkIcon className="flex self-center hover:text-muted-foreground" />
+                  </Link>
+                </div>
+                <span className="text-muted-foreground text-xs">
+                  Web application for building your personal resume. Written in
+                  TypeScript using Next.js
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-y-6">
+          <span className="text-sm">Education</span>
+          <div className="flex flex-col gap-y-4">
+            <div className="flex flex-row justify-start">
+              <div className="w-96">
+                <span className="text-muted-foreground text-sm">
+                  2018 - 2022
+                </span>
+              </div>
+              <div className="flex flex-col gap-y-1 w-full">
+                <span className="text-sm">BSc in Computer Science</span>
+                <span className="text-muted-foreground text-xs">
+                  Wilfrid Laurier University
+                </span>
+                <span className="text-muted-foreground text-xs">
+                  Waterloo, ON
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-y-6">
+          <span className="text-sm">Contact</span>
+          <div className="flex flex-col gap-y-2">
+            <div className="flex flex-row justify-start">
+              <div className="w-96">
+                <span className="text-muted-foreground text-sm">LinkedIn</span>
+              </div>
+              <div className="flex flex-col w-full">
+                <Link
+                  href={"https://www.linkedin.com/in/kyle-papizewski/"}
+                  className="text-sm hover:underline"
+                >
+                  Kyle Papizewski
+                </Link>
+              </div>
+            </div>
+            <div className="flex flex-row justify-start">
+              <div className="w-96">
+                <span className="text-muted-foreground text-sm">Github</span>
+              </div>
+              <div className="flex flex-col w-full">
+                <Link
+                  href={"https://github.com/kylepaps"}
+                  className="text-sm hover:underline"
+                >
+                  kylepaps
+                </Link>
+              </div>
+            </div>
+            <div className="flex flex-row justify-start">
+              <div className="w-96">
+                <span className="text-muted-foreground text-sm">Email</span>
+              </div>
+              <div className="flex flex-col w-full">
+                <span className="text-sm">kylepaps@outlook.com</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
