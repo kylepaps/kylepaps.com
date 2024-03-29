4d2688d3eb085b9cc02f116d89bc4c9ffabda6f4
Kyle Papizewski <kyle@Kyles-MacBook-Pro.local>
2024-03-29 15:06:26 -0400

    smallest change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,13 +48,13 @@ export default function Home() {
           <span className="text-sm">About</span>
           <div className="flex flex-col gap-y-4">
             <span className="text-muted-foreground text-sm">
-              I&#39;m a software developer located in Toronto. Currently I&#39;m
-              working with MLSE as a Junior Backend Engineer, where I&#39;m able
-              to build applications for the Toronto Maple Leafs, Toronto
-              Raptors, and associated fans. Aside from software development, I
-              enjoy running, golf, basketball, and travel. In 2023 I ran my
-              first marathon. This year I&#39;m training for the Toronto
-              Waterfront Marathon with a 3:45 goal.
+              I&#39;m a software developer located in Toronto. Currently working
+              with MLSE as a Junior Backend Engineer, where I&#39;m able to
+              build applications for the Toronto Maple Leafs, Toronto Raptors,
+              and associated fans. Aside from software development, I enjoy
+              running, golf, basketball, and travel. In 2023 I ran my first
+              marathon. This year I&#39;m training for the Toronto Waterfront
+              Marathon with a 3:45 goal.
             </span>
           </div>
         </div>
