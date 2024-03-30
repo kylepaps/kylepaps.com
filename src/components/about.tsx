f53cd6ceabc9ec022bdadd9a2669511625d87b0b
Kyle Papizewski <kyle@Kyles-MacBook-Pro.local>
2024-03-30 11:02:03 -0400

    Update about + favicon

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -6,13 +6,13 @@ const About = () => {
       <span className="text-sm">About</span>
       <div className="flex flex-col gap-y-4">
         <span className="text-muted-foreground text-sm">
-          I&#39;m a software developer located in Toronto. Currently working
-          with MLSE as a Junior Backend Engineer, where I&#39;m able to build
-          applications for the Toronto Maple Leafs, Toronto Raptors, and
-          associated fans. Aside from software development, I enjoy running,
-          golf, basketball, and travel. In 2023 I ran my first marathon. This
-          year I&#39;m training for the Toronto Waterfront Marathon with a 3:45
-          goal.
+          Hello! I&#39;m a software developer located in Toronto. I&#39;m
+          currently working with MLSE as a Junior Backend Engineer, where
+          I&#39;ve been able to build applications for the Toronto Maple Leafs,
+          Toronto Raptors, and associated fans. Aside from software development,
+          I enjoy running, golf, basketball, and travel. In 2023, I ran my first
+          marathon :). This year, I&#39;m training for the Toronto Waterfront
+          Marathon with a 3:45 goal.
         </span>
       </div>
     </div>
