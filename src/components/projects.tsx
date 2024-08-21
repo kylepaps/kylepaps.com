4e5b4b65920987cc62ab4f1ef1052a9d1dbc5904
Kyle Papizewski <kylepaps77@gmail.com>
2024-08-20 20:14:26 -0400

    Add rollout.fyi to projects list

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -6,10 +6,31 @@ const Projects = () => {
   return (
     <div className="flex flex-col gap-y-6">
       <span className="text-sm">Projects</span>
+      <div className="flex flex-row justify-start">
+        <div className="w-80 sm:w-96">
+          <span className="text-muted-foreground text-sm">In Progress</span>
+        </div>
+        <div className="flex flex-col gap-y-1 w-full">
+          <div className="flex flex-row gap-x-2">
+            <span className="text-sm">Rollout</span>
+            <Link
+              href={"https://www.rollout.fyi/"}
+              className="flex items-center"
+            >
+              <ExternalLinkIcon className="flex self-center hover:text-muted-foreground" />
+            </Link>
+          </div>
+          <span className="text-muted-foreground text-xs">
+            A marketing application created for SaaS companies to enhance
+            product visibility and build SEO backlinks. It includes a curated
+            list of over 120 directories, communities, and review sites.
+          </span>
+        </div>
+      </div>
       <div className="flex flex-col gap-y-4">
         <div className="flex flex-row justify-start">
           <div className="w-80 sm:w-96">
-            <span className="text-muted-foreground text-sm">Ongoing</span>
+            <span className="text-muted-foreground text-sm">Paused</span>
           </div>
           <div className="flex flex-col gap-y-1 w-full">
             <div className="flex flex-row gap-x-2">
@@ -22,8 +43,9 @@ const Projects = () => {
               </Link>
             </div>
             <span className="text-muted-foreground text-xs">
-              Web application for building your personal resume. Written in
-              TypeScript using Next.js
+              A web application designed for creating personalized resumes. It
+              offers six customizable templates, allowing you to adjust
+              sections, fonts, spacing, and margins to suit your preferences.
             </span>
           </div>
         </div>
